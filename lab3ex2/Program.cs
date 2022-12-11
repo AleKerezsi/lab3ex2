ef0a72f4d23f37caa601c7b1b3da0e493e69e6e3
alexandra.kerezsi.18@gmail.com <alexandra.kerezsi.18@gmail.com>
2022-12-11 23:37:23 +0200

    Add project files.

---
+++ lab3ex2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab3ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Ex 2 Scrieti o functie care va determina daca un numar este sau nu numar prim. Apelati-o si afisati-i rezultatul */
+            Console.WriteLine("Introduceti n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            VerificaNumarPrim(n);
+        }
+
+        public static void VerificaNumarPrim(int x) 
+        {
+            int numarDivizori = 0;
+            for (int i = 1; i <= x; i++)
+            {
+                if (x % i == 0)
+                {
+                    numarDivizori = numarDivizori + 1;
+                }
+            }
+            if (numarDivizori == 2) Console.WriteLine("Numarul " + x + " este prim");
+            else Console.WriteLine("Numarul " + x + " nu este prim");
+        }
+    }
+}
